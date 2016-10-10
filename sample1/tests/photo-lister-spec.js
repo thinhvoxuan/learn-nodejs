9ad4292af261e9455716215b45872cc2292ec54c
thinhvoxuan <thinhvoxuan@gmail.com>
2016-10-10 17:47:39 +0700

    init sample1 with mocha-chai & fake stub

---
+++ sample1/tests/photo-lister-spec.js
@@ -0,0 +1,8 @@
+var expect      = require('chai').expect,
+  PhotoLister = require('../src/photo-lister');
+
+describe('PhotoLister', function() {
+  it('should exist', function() {
+    expect(PhotoLister).not.to.be.undefined;
+  });
+});
