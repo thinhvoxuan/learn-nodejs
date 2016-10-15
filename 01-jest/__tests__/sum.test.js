01c2f689629ee9597daabe4d21a874dcfe37b45d
thinhvoxuan <thinhvoxuan@gmail.com>
2016-10-15 12:28:14 +0700

    01-jest function

---
+++ 01-jest/__tests__/sum.test.js
@@ -0,0 +1,10 @@
+import {sum, minus} from '../src/sum';
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(3)
+})
+
+
+test('minus 2 - 1 to equal 1', () => {
+    expect(minus(2,1)).toBe(1)
+})
--- 01-mocha-chai/tests/photo-lister-spec.js
+++
@@ -1,8 +0,0 @@
-var expect      = require('chai').expect,
-  PhotoLister = require('../src/photo-lister');
-
-describe('PhotoLister', function() {
-  it('should exist', function() {
-    expect(PhotoLister).not.to.be.undefined;
-  });
-});
