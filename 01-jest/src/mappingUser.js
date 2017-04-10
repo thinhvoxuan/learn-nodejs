2f768c9dd528babc38e9c05005ccda42f95b91d0
thinhvoxuan <thinhvoxuan@gmail.com>
2017-04-11 00:16:16 +0700

    add result

--- 01-jest/src/mappingUser.js
+++ 01-jest/src/mappingUser.js
@@ -1,3 +1,11 @@
-export function mapping(listMember, listToen) {
-    return [ 'token-01' ];
+export function mapping(listMember, listToken) {
+    var result = [];
+    listToken.forEach((obj, idx) => {
+        let token = Object.keys(obj)[0]
+        let userId = obj[token].userId;
+        if (listMember.indexOf(userId) != -1) {
+            result.push(token)
+        }
+    })
+    return result;
 }
